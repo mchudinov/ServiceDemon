aaf6b53ea15a8b00d03f3ff183d5b8cd6f2adf33
Mikael Chudinov <mikael.chudinov@datametrix.no>
2014-12-10 16:44:23 +0100

    Initial commit.

---
+++ ServiceDemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceDemon
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
