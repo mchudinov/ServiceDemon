f4354b09aff0cacaaa6d1a88719abc6edda626c4
Mikael Chudinov <mikael@chudinov.net>
2014-12-14 01:20:51 +0100

    Add demon.sh

---
+++ ServiceDemon/demon.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DEMON_NAME="MyDemon"
+LOCK_FILE="/tmp/"${DEMON_NAME}".lock"
+
+usage()
+{
+    echo "$0 (start|stop)"
+}
+
+stop()
+{
+    if [ -e ${LOCK_FILE} ]
+    then
+        _pid=$(cat ${LOCK_FILE})
+        kill $_pid
+        rt=$?
+        if [ "$rt" == "0" ]
+        then
+                echo "Demon stop"
+        else
+                echo "Error stop demon"
+        fi
+    else
+        echo "Demon is not running"
+    fi
+}
+
+start()
+{
+    mono-service -l:${LOCK_FILE} ./ServiceDemon.exe
+}
+
+case $1 in
+    "start")
+            start
+            ;;
+    "stop")
+            stop
+            ;;
+    *)
+            usage
+            ;;
+esac
+exit
